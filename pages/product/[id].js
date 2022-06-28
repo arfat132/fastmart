f5d977bdb5849d22ba4dc0e72b742c20dd1056df
arfat132 <arfatbegum90@gmail.com>
2022-06-28 18:11:11 +0300

    product details page added

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -14,34 +14,35 @@ export default function ProductScreen() {
   }
   return (
     <Layout title={product.name}>
-      <div className="pb-2">
+      <div className="pb-12">
         <Link href="/">back to products</Link>
       </div>
-      <div className="grid md:grid-cols-4 md:gap-3">
-        <div className="md:col-span-2">
+      <div className="grid md:grid-cols-2 px-12">
+        <div className="">
           <img
             src={product.img}
             alt={product.name}
-            className="rounded shadow w-80"
+            className="rounded shadow w-[600px] h-[500px]"
           />
         </div>
-        <div>
+        <div className='relative mr-12'>
           <ul>
             <li>
-              <h1 className="text-lg">{product.name}</h1>
+              <h1 className="text-xl font-bold mb-2">{product.name}</h1>
             </li>
             <li>Category: {product.category}</li>
             <li>Brand: {product.seller}</li>
-            <li>
-              {product.ratings} of {product.ratingsCount} reviews
-            </li>
-            <li>${product.price}</li>
             <li className="mb-2 flex justify-between">
+              <div className='text-2xl font-bold mt-2'>{product.ratings}</div>
+              <div>{product.ratingsCount} reviews</div>
+            </li>
+            <li className="mb-6 flex justify-between">
               <div>Status</div>
               <div>{product.stock > 0 ? 'In stock' : 'Unavailable'}</div>
             </li>
+            <li className='text-2xl font-bold mb-6'>${product.price}</li>
             <li>Description: {product.description}</li>
-            <button className="primary-button w-full">Add to cart</button>
+            <button className="w-full bottom-0 absolute bg-teal-600 py-2 text-white">Add to cart</button>
           </ul>
         </div>
         <div>
