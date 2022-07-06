90778665e012f99335fb96387eebec3f3b823102
arfat132 <arfatbegum90@gmail.com>
2022-07-06 12:50:23 +0300

    paypal gateway implemented

--- pages/_app.js
+++ pages/_app.js
@@ -2,18 +2,20 @@ import '../styles/globals.css'
 import { StoreProvider } from '../utils/store'
 import { SessionProvider, useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
-
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
       <StoreProvider>
+        <PayPalScriptProvider deferLoading={true}>
       {Component.auth ? (
           <Auth>
             <Component {...pageProps} />
           </Auth>
         ) : (
           <Component {...pageProps} />
-        )}
+          )}
+          </PayPalScriptProvider>
       </StoreProvider>
     </SessionProvider>
   )
